a5c7365ba9853c09885cc3e27f13963eb3f77cc5
Daniel Docki <daniel.docki@gmail.com>
2011-05-17 13:56:10 -0300

    plugin social

--- wp-content/themes/my/inc/core/widgets.php
+++ wp-content/themes/my/inc/core/widgets.php
@@ -138,7 +138,180 @@ class Themetation_Twitter_Widget extends WP_Widget {
 
 }
 
+
+/*
+ * Twitter e RSS Count Widget class.
+ */
+class tz_rsstwitter_widget extends WP_Widget {
+
+	/* ---------------------------- */
+	/* -------- Widget setup -------- */
+	/* ---------------------------- */
+	
+	function TZ_RssTwitter_Widget() {
+	
+		/* Widget settings */
+		$widget_ops = array( 'classname' => 'tz_rsstwitter_widget', 'description' => __('A widget that counts how many RSS subscribers and Twitter followers you have.', 'framework') );
+
+		/* Widget control settings */
+		//$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'tz_ad300_widget' );
+
+		/* Create the widget */
+		$this->WP_Widget( 'tz_rsstwitter_widget', __('Custom RSS and Twitter counter', 'framework'), $widget_ops, $control_ops );
+	}
+
+	/* ---------------------------- */
+	/* ------- Display Widget -------- */
+	/* ---------------------------- */
+	
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		/* Our variables from the widget settings. */
+		$title = apply_filters('widget_title', $instance['title'] );
+		$username = $instance['username'];
+		$refresh_text = $instance['refresh_text'];
+		$twitter = $instance['twitter'];
+
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+
+		/* Display the widget title if one was input (before and after defined by themes). */
+		if ( $title )
+			echo $before_title . $title . $after_title;
+		
+		// RSS Code
+		$theurl = file_get_contents('https://feedburner.google.com/api/awareness/1.0/GetFeedData?uri='. $username);
+		$begin = 'circulation="'; $end = '"';
+		$page = $theurl;
+		$parts = explode($begin,$page);
+		$page = $parts[1];
+		$parts = explode($end,$page);
+		$fbcount = $parts[0];
+		if($fbcount == '0' || $fbcount == '' ) { $fbcount = $refresh_text; }
+		
+		
+		//Twitter code
+		$twit = file_get_contents("http://twitter.com/users/show/".$twitter.".xml");
+		$begin = '<followers_count>'; $end = '</followers_count>';
+		$page = $twit;
+		$parts = explode($begin,$page);
+		$page = $parts[1];
+		$parts = explode($end,$page);
+		$tcount = $parts[0];
+		if($tcount == '') { $tcount = '0'; }
+	
+	
+	?>
+        <div class="rss_widget">
+        
+            <span class="icon"><a href="http://feeds.feedburner.com/<?php echo $username; ?>"><img src="<?php bloginfo('template_directory'); ?>/styles/images/rss_icon.png" alt="rss" /></a></span>
+                                
+            <div class="details">
+                
+                <a href="http://feeds.feedburner.com/<?php echo $username; ?>">
+                <span class="count"><?php echo $fbcount; ?></span>
+                <span class="desc"><?php _e('Assinar', 'framework'); ?></span>
+                </a>
+                
+            </div><!--details-->
+            
+        </div><!--rss_widget-->
+        
+        <?php
+		
+		?>
+        <div class="twitter_widget">
+            
+            <span class="icon"><a href="<?php echo "http://www.twitter.com/$twitter"; ?>"><img src="<?php bloginfo('template_directory'); ?>/styles/images/twitter_icon.png" alt="twitter" /></a></span>
+            
+            <div class="details">
+                
+                <a href="<?php echo "http://www.twitter.com/$twitter"; ?>">
+                <span class="count"><?php echo $tcount; ?></span>
+                <span class="desc"><?php _e('Siga-nós', 'framework'); ?></span>
+                </a>
+                
+            </div><!--details-->
+            
+        </div><!--twitter_widget-->
+                        
+        <?php
+
+		/* After widget (defined by themes). */
+		echo $after_widget;
+	}
+
+	/* ---------------------------- */
+	/* ------- Update Widget -------- */
+	/* ---------------------------- */
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		/* Strip tags for title and name to remove HTML (important for text inputs). */
+		$instance['title'] = strip_tags( $new_instance['title'] );
+
+		/* No need to strip tags */
+		$instance['username'] = $new_instance['username'];
+		$instance['refresh_text'] = $new_instance['refresh_text'];
+		
+		$instance['twitter'] = $new_instance['twitter'];
+
+		return $instance;
+	}
+	
+	/* ---------------------------- */
+	/* ------- Widget Settings ------- */
+	/* ---------------------------- */
+	
+	/**
+	 * Displays the widget settings controls on the widget panel.
+	 * Make use of the get_field_id() and get_field_name() function
+	 * when creating your form elements. This handles the confusing stuff.
+	 */
+	
+	function form( $instance ) {
+	
+		/* Set up some default widget settings. */
+		$defaults = array(
+		'title' => '',
+		'username' => "premiumpixels",
+		'refresh_text' => '1000+',
+		'twitter' => 'ormanclark',
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+		<!-- Widget Title: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'framework') ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" />
+		</p>
+
+		<!-- Ad image url: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'username' ); ?>"><?php _e('RSS Acount:', 'framework') ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'username' ); ?>" name="<?php echo $this->get_field_name( 'username' ); ?>" value="<?php echo $instance['username']; ?>" />
+		</p>
+        
+        <!-- Ad image url: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'refresh_text' ); ?>"><?php _e('RSS Refresh Text:', 'framework') ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'refresh_text' ); ?>" name="<?php echo $this->get_field_name( 'refresh_text' ); ?>" value="<?php echo $instance['refresh_text']; ?>" />
+		</p>
+		
+		<!-- Ad twitter url: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'twitter' ); ?>"><?php _e('Twitter Acount:', 'framework') ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'twitter' ); ?>" name="<?php echo $this->get_field_name( 'twitter' ); ?>" value="<?php echo $instance['twitter']; ?>" />
+		</p>
+		
+	<?php
+	}
+}
+
 // register widget classess
 add_action('widgets_init', create_function('', 'return register_widget("Themetation_Flickr_Widget");'));
 add_action('widgets_init', create_function('', 'return register_widget("Themetation_Twitter_Widget");'));
+add_action('widgets_init', create_function('', 'return register_widget("tz_rsstwitter_widget");'));
 ?>
--- wp-content/themes/my/single.php
+++ wp-content/themes/my/single.php
@@ -13,19 +13,8 @@
                         
                         <div id="extra">
                         	<div class="related">
-                            	<h3><?php _e('Related Posts','my') ?></h3>
+                            	<h3><?php _e('Posts Relacionados','my') ?></h3>
                                 <ul><?php include('related.php'); ?></ul>
-                          </div>
-                          <div class="social">
-
-                          	<a href="http://del.icio.us/post?url=<?php the_permalink(); ?>&amp;title=<?php the_title(); ?>" id="delicious-button" title="Delicious"><?php _e('Delicious','my')?></a>
-                              <a href="http://www.stumbleupon.com/submit?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" id="stumble-button" title="Stumble"><?php _e('Stumble','my')?></a>
-                              <a href="http://digg.com/submit?phase=2&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" id="digg-button" title="Digg"><?php _e('digg<','my')?>/a>
-                              <a href="http://www.reddit.com/submit?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" id="redd-button" title="Reddit"><?php _e('Reddit','my')?></a>
-                              <a href="http://www.mixx.com/submit?page_url=<?php the_permalink(); ?>" id="mixx-button" title="Mixx"><?php _e('Mixx','my')?></a>
-                              <a href="http://twitter.com/home?status=<?php the_permalink(); ?>" id="twitt-button" title="Twitter"><?php _e('Twitter','my')?></a>
-                              <a href="<?php the_permalink(); ?>feed" id="rss2-button" title="Subscribe"><?php _e('Subscribe','my')?></a>
-
                           </div>
                           <div class="clear"></div>
                         </div>
